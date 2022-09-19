6080d02f926a9dec316f295bc80fed432a11f2f7
Andrew W. Burger <andrew.burger@awbtech.org>
2022-09-19 15:44:04 -0400

    Added SEO Meta and Settings Deployment Data

--- awbtechllc/settings.py
+++ awbtechllc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2b5nqexa^#8%prf21kgc$l1!(ah%45vyx(-&gxs$1o2^kvo=vj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['24.199.66.239', 'awbtech.org', 'www.awbtech.org', 'localhost']
 
 
 # Application definition
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
